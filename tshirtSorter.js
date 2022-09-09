883ae8da7f37142d837a8185e6cbb058e7d752a0
Adam Cox <adamcox.dev@gmail.com>
2022-09-09 10:24:33 -0600

    Final commit for morning challenge

--- tshirtSorter.js
+++ tshirtSorter.js
@@ -1,1 +1,28 @@
 // Write your solution below:
+let arr = [];
+let finalArr = [];
+let answer = '';
+
+const sortShirts = (string) => {
+  let arr = string.split('');
+ 
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === 's'){
+        finalArr.push(arr[i])
+    }
+  }
+  for(let j = 0; j < arr.length; j++){
+    if(arr[j] === 'm'){
+        finalArr.push(arr[j])
+    }
+  }
+  for(let k = 0; k < arr.length; k++){
+    if(arr[k] === 'l'){
+        finalArr.push(arr[k])
+    }
+  }
+  answer = finalArr.join() 
+  console.log(answer);
+}
+
+sortShirts('slsllsmmmm');
